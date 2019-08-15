915094989ab666e171345c7a3b3f235c21891a0e
janani2000 <46857506+janani2000@users.noreply.github.com>
2019-08-15 20:40:45 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+# alphabet
+#include<stdio.h>
+int main()
+{
+char c;
+clrscr();
+printf("Enter a character:");
+scanf("%c",&c);
+{
+if((c>='a' &&c<='z')||(c>='A' &&c<='Z'))
+printf("%c is an alphabet",c);
+else
+printf("%c is No alphabet",c);
+}
+getch();
+}
